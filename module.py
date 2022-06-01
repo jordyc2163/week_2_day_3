7828dd56fb4c61e7925066db5fcd8aa2adac8e04
jordyc2163 <jordycaceres00@gmail.com>
2022-06-01 17:23:42 -0400

    first commit

---
+++ module.py
@@ -0,0 +1,13 @@
+def printName(name):
+    print(f"Hello Mr/Ms {name}...we've been waiting for you!")
+
+
+def squarefunc(length, width):
+    area = length * width
+    return area
+
+
+def givecircum(diameter):
+    from math import pi
+    circumference = 2 * pi * (diameter/2)
+    return circumference
